32907d2cd2241b7e5da82e93ae8dfa0d283fa7a6
racheliver <racheliver@gmail.com>
2023-10-06 19:58:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", date: new Date(2021, 2, 7), amount: "222$" },
+    { title: "Car Insurance2", date: new Date(2021, 2, 8), amount: "111$" },
+    { title: "Car Insurance3", date: new Date(2021, 2, 9), amount: "445$" },
+    { title: "Car Insurance4", date: new Date(2021, 2, 10), amount: "555$" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      {expenses.map((expense, index) => (
+        <ExpenseItem
+          key={index} // It's important to provide a unique key when rendering a list of components.
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+        />
+      ))}
     </div>
   );
 }
