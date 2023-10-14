60bea276255f937f2612da575691cefe54e25905
racheliver <racheliver@gmail.com>
2023-10-14 12:37:45 +0300

    Outputting Conditional Content

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -7,20 +7,25 @@ import ExpensesFilter from './ExpensesFilter';
 function Expense(props) {
   const [yearFilterd, setYearFilter] = useState('2022');
   const filteredExpenses  = props.items.filter(item => {
-    return item.date.getFullYear().toString() === yearFilterd
+    return item.date.getFullYear().toString() === yearFilterd;
   })
+  
+  let expensesContent = <p>No expenses found</p>
 
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        date={expense.date}
+        amount={expense.amount}
+      />
+    ))
+  };
   return (
     <Card className="expenses">
       <ExpensesFilter selected={yearFilterd} onSetYearFilter={setYearFilter} />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          amount={expense.amount}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 }
